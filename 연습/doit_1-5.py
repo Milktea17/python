991b0fcea57b38dacaa17e1ce449a89e41a2e555
letter <llilljs@naver.com>
2022-08-21 14:26:04 +0900

    폴더 이동

---
+++ 연습/doit_1-5.py
@@ -0,0 +1,35 @@
+"""
+----------------------
+01-5 파이썬 둘러보기
+----------------------
+"""
+
+#조건문 IF
+
+"""
+a=3
+if a>1:
+    print("a is greater than 1")
+"""
+
+#반복문 for
+"""
+for a in [1,2,3]:
+    print(a)
+"""
+
+#반복문 while
+"""
+i = 0
+while i < 3:
+    i = i+1
+    print(i)
+"""
+
+#함수
+"""
+def add(a,b):
+    return a+b
+
+print(add(3,4))
+"""
