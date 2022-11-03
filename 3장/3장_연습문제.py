5c7a34912ed052686d92a442b6f196c865dc14f1
letter <llilljs@naver.com>
2022-11-03 23:01:41 +0900

    3장 완료

--- 3장/3장_연습문제.py
+++ 3장/3장_연습문제.py
@@ -1,5 +1,8 @@
 #Q1.다음 코드의 결괏값은 무엇일까?
 
+from re import S
+
+
 a = "Life is too short, you need python"
 
 if "wife" in a: print("wife")
@@ -43,7 +46,41 @@ while num <=5:
 #***
 #****
 #*****
+print("="*40)
+
 
 #Q4.for문을 사용해 1부터 100까지의 숫자를 출력해 보자.
 for i in range(1,101):
     print(i)
+#A4. 1부터 100까지 각 라인마다 출력
+print("="*40)
+
+
+#Q5.A 학급에 총 10명의 학생이 있다. 이 학생들의 중간고사 점수는 다음과 같다.
+#[70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+#for문을 사용하여 A 학급의 평균 점수를 구해 보자.
+
+score=[70, 60, 55, 75, 95, 90, 80, 80, 85, 100]
+sum=0
+for s in score :
+    sum+=s
+
+print("평균점수 : %d"%(sum/10))
+#print("평균점수 : %d"%(sum/len(score))) //풀이대로
+
+#A5. 평균점수 : 79
+print("="*40)
+
+#Q6. 리스트 중에서 홀수에만 2를 곱하여 저장하는 다음 코드가 있다.
+#위 코드를 리스트 내포(list comprehension)를 사용하여 표현해 보자.
+
+numbers = [1, 2, 3, 4, 5]
+result = []
+for n in numbers:
+    if n % 2 == 1:
+        result.append(n*2)
+        
+result = [n*2 for n in numbers if n%2 == 1]
+print(result)
+
+#A6.[2, 6, 10]
